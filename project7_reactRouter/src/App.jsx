d19c612377a3e7b71c0fdca483bc1e37c19abfb1
vishalsharma123321 <vishalsharma2212003@gmail.com>
2024-07-16 16:46:20 +0530

    new projrct 7

---
+++ project7_reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  <h1 className=' bg-green-500 p-5 text-black rounded-xl font-mono font-bold '>Hi There</h1>
+  </>
+  )
+}
+
+export default App
